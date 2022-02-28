0f3833a1a0a2a8bd758c313516d97b59637f6c71
tonggui <1519003902@qq.com>
2022-02-28 13:09:44 +0800

    feat:删除废弃代码

--- src/router/index.js
+++ src/router/index.js
@@ -6,18 +6,6 @@ Vue.use(Router)
 /* Layout */
 import Layout from '../views/layout/Layout'
 
-/**
- * hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
- *                                if not set alwaysShow, only more than one route under the children
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noredirect           if `redirect:noredirect` will no redirct in the breadcrumb
- * name:'router-name'             the name is used by <keep-alive> (must set!!!)
- * meta : {
-    title: 'title'               the name show in submenu and breadcrumb (recommend set)
-    icon: 'svg-name'             the icon show in the sidebar,
-  }
- **/
 export const constantRouterMap = [
   {path: '/login', component: () => import('@/views/login/index'), hidden: true},
   {path: '/404', component: () => import('@/views/404'), hidden: true},
@@ -49,93 +37,93 @@ export const constantRouterMap = [
 export const asyncRouterMap = [
 
   {
-    path: '/pms',
+    path: '/medication',
     component: Layout,
-    redirect: '/pms/Medication',
-    name: 'pms',
+    redirect: '/medication/list',
+    name: 'medication',
     meta: {title: 'Medication', icon: 'ums-menu'},
     children: [{
-      path: 'product',
-      name: 'product',
-      component: () => import('@/views/pms/product/index'),
+      path: 'medicationList',
+      name: 'medicationList',
+      component: () => import('@/views/medication/list/index'),
       meta: {title: 'Records', icon: 'product-list'}
     },
       {
-        path: 'addProduct',
-        name: 'addProduct',
-        component: () => import('@/views/pms/product/add'),
+        path: 'addMedication',
+        name: 'addMedication',
+        component: () => import('@/views/medication/add/index'),
         meta: {title: 'Search', icon: 'product-add'}
       },
     ]
   },
   {
-    path:'/ums',
+    path:'/vital-signs',
     component: Layout,
-    redirect: '/ums/admin',
-    name: 'ums',
+    redirect: '/vital-signs/list',
+    name: 'vital-signs',
     meta: {title: 'Vital signs', icon: 'ums-resource'},
     children: [
       {
-        path: 'admin',
-        name: 'admin',
-        component: () => import('@/views/ums/admin/index'),
+        path: 'list',
+        name: 'list',
+        component: () => import('@/views/vital-signs/list/index'),
         meta: {title: 'Vital signs list', icon: 'order-return-reason'}
       },
       {
-        path: 'menu',
-        name: 'menu',
-        component: () => import('@/views/ums/menu/index'),
+        path: 'mgmt',
+        name: 'mgmt',
+        component: () => import('@/views/vital-signs/add/index'),
         meta: {title: 'Vital MGMT.', icon: 'ums-menu'}
       },
     ]
   },
 
   {
-    path: '/oms',
+    path: '/diet',
     component: Layout,
-    redirect: '/oms/order',
-    name: 'oms',
+    redirect: '/diet/list',
+    name: 'diet',
     meta: {title: 'Diet', icon: 'order'},
     children: [
       {
-        path: 'order',
-        name: 'order',
-        component: () => import('@/views/oms/order/index'),
+        path: 'list',
+        name: 'list',
+        component: () => import('@/views/diet/list/index'),
         meta: {title: 'Diet list', icon: 'order-return'}
       },
       {
-        path: 'orderDetail',
-        name: 'orderDetail',
-        component: () => import('@/views/oms/order/orderDetail'),
+        path: 'dietDetail',
+        name: 'dietDetail',
+        component: () => import('@/views/diet/dietDetail'),
         meta: {title: 'Diet detail'},
         hidden:true
       },
       {
-        path: 'orderSetting',
-        name: 'orderSetting',
-        component: () => import('@/views/oms/order/setting'),
+        path: 'mgmt',
+        name: 'mgmt',
+        component: () => import('@/views/diet/add/index'),
         meta: {title: 'Diet MGMT.', icon: 'ums-menu'}
       },
     ]
   },
   {
-    path:'/sms',
+    path:'/notes',
     component: Layout,
-    redirect: '/sms/coupon',
-    name: 'sms',
+    redirect: '/notes/list',
+    name: 'notes',
     meta: {title: 'Notes', icon: 'sms'},
     children: [
       {
-        path: 'flash',
-        name: 'flash',
-        component: () => import('@/views/sms/flash/index'),
+        path: 'list',
+        name: 'list',
+        component: () => import('@/views/notes/list/index'),
         meta: {title: 'Notes List', icon: 'sms-flash'}
       },
 
       {
-        path: 'brand',
-        name: 'homeBrand',
-        component: () => import('@/views/sms/brand/index'),
+        path: 'mgmt',
+        name: 'mgmt',
+        component: () => import('@/views/notes/add/index'),
         meta: {title: 'Notes MGMT.', icon: 'ums-menu'}
       },
     ]
@@ -148,15 +136,15 @@ export const asyncRouterMap = [
     meta: {title: 'COMM.', icon: 'ums-role'},
     children: [
       {
-        path: 'admin',
-        name: 'admin',
-        component: () => import('@/views/ums/admin/index'),
+        path: 'list',
+        name: 'list',
+        component: () => import('@/views/comm/list/index'),
         meta: {title: 'COMM. list', icon: 'sms-subject'}
       },
       {
-        path: 'role',
-        name: 'role',
-        component: () => import('@/views/ums/role/index'),
+        path: 'appt',
+        name: 'appt',
+        component: () => import('@/views/comm/appt/index'),
         meta: {title: 'APPT. list', icon: 'ums-admin'}
       }
     ]
